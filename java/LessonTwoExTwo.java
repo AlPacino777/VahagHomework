5e8317371fb97d0e971f7a1102c7b5cbe2dc64b8
AlPacino777 <vahagn_harutyunyan@mail.ru>
2020-12-10 11:09:21 +0400

    LessonTwoExTwo

---
+++ java/LessonTwoExTwo.java
@@ -0,0 +1,14 @@
+public class LessonTwoExTwo {
+    public static void main(String[] args) {
+        double A = 3.8;
+        int N = 6;
+        double sum = 1;
+        double k = -1;
+
+        for (int i = 0; i < N; i++) {
+            k = k * A;
+            sum += k;
+        }
+        System.out.println(sum);
+    }
+}
