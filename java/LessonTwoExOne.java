1af65f126adbedf642f8835c8007011e73bcc14c
AlPacino777 <vahagn_harutyunyan@mail.ru>
2020-12-10 11:01:07 +0400

    LessonTwoExOne

---
+++ java/LessonTwoExOne.java
@@ -0,0 +1,11 @@
+public class LessonTwoExOne {
+    public static void main(String[] args) {
+        double price = 2.5;
+        int count = 2;
+
+        while (count < 11) {
+            System.out.println(price * count);
+            count += 2;
+        }
+    }
+}
