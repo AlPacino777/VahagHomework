c8ad3ed93e9266a28902522d45ed8073981bea08
AlPacino777 <vahagn_harutyunyan@mail.ru>
2020-12-17 15:18:14 +0400

    LessonThreeExThree

---
+++ out/production/VahagHomework/LessonThreeExThree.java
@@ -0,0 +1,9 @@
+public class LessonThreeExThree {
+    public static void main(String[] args) {
+        int a1a2 = 25;
+        int b = 7;
+
+        System.out.println(a1a2 % 10 * 10 + a1a2 / 10 - b);
+
+    }
+}
